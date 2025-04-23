d78489532f652b2e256504151017d784f3ee753a
Hristina Bozhinova <117430805+bozhinova-hristina@users.noreply.github.com>
2025-04-23 09:57:29 +0200

    homework added

--- G7/Class01/type-assertions/index.ts
+++ G7/Class01/type-assertions/index.ts
@@ -4,27 +4,25 @@ let movieName = "Shawshank Redemtion";
 // movieName = 123 // Error: Type 'number' is not assignable to type 'string'
 
 // Type assertion
-let someValue: any = "This is a string"
+let someValue: any = "This is a string";
 let someValue2 = someValue as string; // casting type to a value. Usefull only when we are SURE of the value's type. Do not overuse it
 
-
 interface Coordinates {
-    lat: number
-    lng: number
-};
-
+  lat: number;
+  lng: number;
+}
 
 let cityCoordinates: Coordinates = {
-    lat: 23123.123,
-    lng: 12311
+  lat: 23123.123,
+  lng: 12311,
 };
 
 let cityCoordinatesSecond: Coordinates = {} as Coordinates;
-console.log(cityCoordinatesSecond)
+console.log(cityCoordinatesSecond);
 
 // .. somewhere below, after we retrieve the lat & lng
 
 cityCoordinatesSecond.lat = 1111.33;
 cityCoordinatesSecond.lng = 2323.44;
 
-console.log(cityCoordinatesSecond)
+console.log(cityCoordinatesSecond);
