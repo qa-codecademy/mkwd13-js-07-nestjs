fd8e0d4e7cbe0fe2858eeb489ecbbf39aa52599c
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-04-25 19:41:27 +0200

    ts break 2

--- G5/class_01_TS_1/examples/01_basic_types.ts
+++ G5/class_01_TS_1/examples/01_basic_types.ts
@@ -34,3 +34,111 @@ userInput = 'hello';
 if (typeof userInput === 'string') {
 	let myString: string = userInput;
 }
+
+export const test: string = 'test';
+
+//  Functions
+
+// void
+function logSomething(): void {
+	console.log('Something');
+
+	// return '' - return is not allowed
+}
+// let result = logSomething();
+
+// Never
+function throwingError(error: string): never {
+	throw new Error(`Unexpected error occurred: ${error}`);
+}
+
+// Null & Undefined
+
+let dogName: string | undefined = undefined;
+dogName = 'Aron';
+
+let dogAge: number | null = null;
+dogAge = 12;
+
+// Object
+
+// let student3: Object = {
+// 	firstName: "Nesto",
+// 	lastName: "Nesto",
+// 	favoriteFood: ''
+// }
+
+let student1: {
+	name: string;
+	age: number;
+	isActive: boolean;
+	tagNumber: string | null;
+	favoriteClass?: string; // favoriteClass: string | undefined;
+} = {
+	name: 'Daniel',
+	age: 20,
+	isActive: true,
+	tagNumber: null,
+};
+
+let student2: {
+	name: string;
+	age: number;
+	isActive: boolean;
+	tagNumber: string | null;
+	favoriteClass?: string;
+} = {
+	name: 'Igor',
+	age: 22,
+	isActive: false,
+	tagNumber: 'ABC123',
+	favoriteClass: 'SQL',
+	// favoriteFood: ''
+};
+
+// Interfaces
+
+interface Student {
+	name: string;
+	age: number;
+	isActive: boolean;
+	tagNumber: string | null;
+	favoriteClass?: string;
+	// writeHomework: () => string // interfaces can define methods
+}
+
+let student3: Student = {
+	name: 'Daniel',
+	age: 20,
+	isActive: true,
+	tagNumber: null,
+};
+
+let student4: Student = {
+	name: 'Igor',
+	age: 22,
+	isActive: false,
+	tagNumber: 'ABC123',
+	favoriteClass: 'SQL',
+	// favoriteFood: ''
+};
+
+// Enums
+export enum Days {
+	Monday = 1, // 1
+	Tuesday, // 2
+	Wednesday, // 3
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday,
+}
+export enum DaysAsWords {
+	Monday = 'Monday',
+	Tuesday = 'Tuesday',
+	Wednesday = 'Wednesday',
+	Thursday = 'Thursday',
+	Friday = 'Friday',
+	Saturday = 'Saturday',
+	Sunday = 'Sunday',
+}
--- G5/class_01_TS_1/examples/index.ts
+++ G5/class_01_TS_1/examples/index.ts
@@ -1,1 +1,9 @@
-console.log('hello from TS');
+import { test, Days, DaysAsWords } from './01_basic_types';
+
+console.log('hello from TS', test);
+
+console.log(Days.Tuesday, DaysAsWords.Tuesday);
+
+for (const day of Object.values(DaysAsWords)) {
+	console.log(day);
+}
