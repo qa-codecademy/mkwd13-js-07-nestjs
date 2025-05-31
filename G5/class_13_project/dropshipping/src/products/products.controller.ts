849c6437c7e51681624525423ae563a39db8bcaa
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-05-31 15:10:57 +0200

    add swagger docs for workshop

--- G5/class_13_project/dropshipping/src/products/products.controller.ts
+++ G5/class_13_project/dropshipping/src/products/products.controller.ts
@@ -10,7 +10,16 @@ import {
 import { ProductsService } from './products.service';
 import { ProductCreateDto } from './dto/product-create.dto';
 import { Product } from './product.entity';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { ICurrentUser } from '../common/types/current-user';
@@ -25,6 +34,13 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new product' })
+  @ApiBody({ type: ProductCreateDto })
+  @ApiCreatedResponse({
+    description: 'The product has been successfully created.',
+    type: Product,
+  })
+  @ApiBadRequestResponse({ description: 'Bad request.' })
   create(
     @Body() productCreateDto: ProductCreateDto,
     @CurrentUser() user: ICurrentUser,
@@ -33,16 +49,35 @@ export class ProductsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Search for products' })
+  @ApiOkResponse({
+    description: 'The products have been successfully searched.',
+    type: [Product],
+  })
   search() {
     return this.productsService.search();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a product by ID' })
+  @ApiOkResponse({
+    description: 'The product has been successfully found.',
+    type: Product,
+  })
+  @ApiNotFoundResponse({ description: 'Product not found.' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a product by ID' })
+  @ApiBody({ type: ProductUpdateDto })
+  @ApiOkResponse({
+    description: 'The product has been successfully updated.',
+    type: Product,
+  })
+  @ApiNotFoundResponse({ description: 'Product not found.' })
+  @ApiBadRequestResponse({ description: 'Bad request.' })
   @UseGuards(JwtAuthGuard, ProductOwnershipGuard)
   update(@Param('id') id: string, @Body() productUpdateDto: ProductUpdateDto) {
     return this.productsService.update(id, productUpdateDto);
